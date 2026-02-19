7df7748cbae6873c6c8a3b6d6dc40cd0b1f4a175
fatdogvitamins <fatdogvitamins@outlook.com>
2026-02-19 16:29:23 -0600

    Hello Git!

--- .py
+++ .py
@@ -3,3 +3,12 @@ import pandas as pd
 readit = open('.jres', 'r')
 data = json.load(readit)
 print(json.dumps(data, indent=int('4'), ensure_ascii=False))
+df = pd.DataFrame({
+    "sonic": {
+        ".": {
+            "": None,
+            "blue": True
+        }
+    }
+})
+[print(df.sonic.to_string(buf=None,max_rows=None)) for _ in range(3)]
